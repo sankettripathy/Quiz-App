86da05a31770f93c4fca67b306d28491d7a920d9
sankettripathy <128065019+sankettripathy@users.noreply.github.com>
2025-09-28 15:19:15 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,4 @@
-// Update this page (the content is just a fallback if you fail to update the page)
+
 
 const Index = () => {
   return (
@@ -12,3 +12,4 @@ const Index = () => {
 };
 
 export default Index;
+
